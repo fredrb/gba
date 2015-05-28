a953a0bde695c49a192b0019ea3e8c5812fc134f
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2015-05-28 01:52:36 -0300

    Still listens to port

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ app.use(express.static(__dirname));
 Due to lates virtual-host update the express apps are no longer being
 started individually - they are all exported as express applications and
 loaded in vhost process
-
+*/
 app.listen(8080, function(){
     console.log("Server running")
 })
-*/
 
-module.exports = app;
+//module.exports = app;
